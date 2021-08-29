d4bd7ec96071e2d02f4e3d645363207609fb8e17
Benedict <benedictiknkeonye@gmail.com>
2021-08-29 14:54:59 +0100

    Feat: completed home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,16 @@
   <div>
     <Navbar />
     <Nuxt />
-    <Footer />
+    <div v-if="!isLoading">
+      <Footer />
+    </div>
   </div>
 </template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['isLoading']),
+  },
+}
+</script>
