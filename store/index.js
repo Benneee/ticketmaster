55851aba671fb19bf265b47c7c5223b3a087e417
Benedict <benedictiknkeonye@gmail.com>
2021-09-01 06:06:47 +0100

    Fix: format comment

--- store/index.js
+++ store/index.js
@@ -64,7 +64,11 @@ export const mutations = {
       (ticket) => ticket.id === ticketId
     )
     const ticketToDecrement = state.tickets[ticketIdx]
-    // Remember I disable the "DecreaseCount" button when the count of a particular ticket is 0, so there's little to no chance 0 will be sent here :)
+    /**
+     * Remember I disable the "DecreaseCount" button
+     * when the count of a particular ticket is 0, so there's little
+     * to no chance 0 will be sent here :)
+     */
     ticketToDecrement.count--
     state.tickets.splice(ticketIdx, 1, ticketToDecrement)
   },
